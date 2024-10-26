75bc513dd3d0b095b4c47629d9da46e3a0c3d493
harshapatil7 <harshapatil.karate2001@gmail.com>
2024-10-26 21:09:56 +0530

    Application Developed

--- backend/config/db.js
+++ backend/config/db.js
@@ -1,12 +1,9 @@
 const mongoose = require('mongoose');
 require('dotenv').config();
 
-
-const MONGO_URI = process.env.MONGO_URI || 'YOUR MONGODB URL';
-
 const connectDB = async () => {
     try {
-        await mongoose.connect(MONGO_URI, {
+        await mongoose.connect(process.env.MONGO_URI, {
             useNewUrlParser: true,
             useUnifiedTopology: true,
             useFindAndModify: false
